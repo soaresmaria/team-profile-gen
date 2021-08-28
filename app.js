90d7f41ebfc6e1bb00a126468f023473eb8dd9bd
Mona Lisa <soaresmaria181@yahoo.com>
2021-08-27 23:48:40 -0400

    added function to engineer,intern and manager

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
